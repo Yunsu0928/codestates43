869ed98ab7ff4f0ea6f888420d9f657aeef1bf20
Yunsu0928 <115795661+Yunsu0928@users.noreply.github.com>
2022-12-16 15:49:39 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# day1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-dhrnzj)
---
+++ index.js
@@ -0,0 +1,14 @@
+// 변수 num에 할당된 값을 변경하여 구구단 n단을 출력해 보세요.
+
+let num; // 변수 선언
+num = 5; // 값의 할당
+
+console.log(num * 1);
+console.log(num * 2);
+console.log(num * 3);
+console.log(num * 4);
+console.log(num * 5);
+console.log(num * 6);
+console.log(num * 7);
+console.log(num * 8);
+console.log(num * 9);
